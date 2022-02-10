6e44e2879f4a5081fdcbed116901fe7a25ebd11f
EricAltez <ericaltez1111@gmail.com>
2022-02-10 09:14:51 -0800

    add functions adn prototipes

--- sort.h
+++ sort.h
@@ -3,52 +3,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 /**
- * print_list - Prints a list of integers
+ * struct listint_s - Doubly linked list node
  *
- * @list: The list to be printed
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
-void print_list(const listint_t *list)
+typedef struct listint_s
 {
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
-
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
 #endif
