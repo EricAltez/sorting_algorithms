a1b8f1b3deb89db13285605bc9c14bef9e3e08a7
EricAltez <ericaltez1111@gmail.com>
2022-02-10 09:41:31 -0800

    asd

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,2 +1,19 @@
-sd
+#include "sort.h"
+
+/**
+ *bubble_sort - sort using bobble method
+ *@array: array to be sorted
+ *@size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{	
+	int i = 0;
+
+	if (size < 2)
+		return;
+
+	
+
+
+}
 
--- sort.h
+++ sort.h
@@ -5,14 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -26,4 +18,13 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
 #endif
