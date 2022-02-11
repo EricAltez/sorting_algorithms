0b82810ae4032810bd7d065ce5e801ea4fbc783a
lulimsky <3337@holbertonschool.com>
2022-02-10 20:03:22 -0800

    0_bubble_sort.c successful

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,14 +6,27 @@
  *@size: size of the array
  */
 void bubble_sort(int *array, size_t size)
-{	
-	int i = 0;
+{
+unsigned int i, j;
+int swap;
 
-	if (size < 2)
-		return;
+for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size - i - 1; j++)
+	{
+		/*
+		 * If the current element is greater than
+		 * the next element of the array, swap them
+		 */
 
-	
-
-
+		if (array[j] > array[j + 1])
+		{
+			/*Swapping the elements*/
+			swap = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = swap;
+			print_array(array, size);
+		}
+	}
 }
-
+}
