453a16b52b4f4b02e88229fb36c034c52b3542f4
EricAltez <ericaltez1111@gmail.com>
2022-02-14 08:23:38 -0800

    corrected file 101

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,31 +1,61 @@
 #include "sort.h"
 
+void move_left(listint_t *curr, listint_t *prev, listint_t **head);
+
 /**
- *cocktail_sort_list - sotrs list
- *@list: dll to be sorted
- */
+* cocktail_sort_list - coctail sort in doubly linked list
+* @list: A Doubly linked list
+*/
+
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *q, *r;
-	int tmp;
+	listint_t *cur, *max = NULL, *min = NULL;
 
-	if (!list || !(*list) || !(*list)->next)
+	if (!list || !(*list) || (*list)->next == NULL)
 		return;
 
-	q = *list;
-	while (q != NULL)
-	{
-		r = q->next;
-		while (r != NULL)
+	cur = *list;
+	do {
+		while (cur->next)
 		{
-			if (q->n > r->n)
-			{
-				tmp = q->n;
-				q->n = r->n;
-				r->n = tmp;
-			}
-			r = r->next;
+			if (cur->n > cur->next->n)
+				move_left(cur->next, cur, list);
+			else
+				cur = cur->next;
 		}
-		q = q->next;
-	}
+		max = cur;
+		while (cur->prev != min)
+		{
+			if (cur->n < cur->prev->n)
+				move_left(cur, cur->prev, list);
+			else
+				cur = cur->prev;
+		}
+		min = cur;
+	} while (min != max);
+}
+
+/**
+* move_left - swaps two members of a list
+*
+* @curr: current node
+* @prev: previous node
+* @head: head of list
+*/
+void move_left(listint_t *curr, listint_t *prev, listint_t **head)
+{
+	listint_t *swap1 = curr->next;
+	listint_t *swap2 = prev->prev;
+
+	if (swap1 != NULL)
+		swap1->prev = prev;
+	if (swap2 != NULL)
+		swap2->next = curr;
+	curr->prev = swap2;
+	prev->next = swap1;
+	curr->next = prev;
+	prev->prev = curr;
+	if (*head == prev)
+		*head = curr;
+	print_list(*head);
 }
